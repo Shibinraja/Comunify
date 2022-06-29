b21afb24e880d29c6834b3ac69e7daeb0220b09b
Armaan Mathew <armaan.mathew@neoito.com>
2022-06-29 12:37:52 +0530

    bugfix: fixed build errors

--- src/hooks/useLoading.ts
+++ src/hooks/useLoading.ts
@@ -1,9 +1,9 @@
 import { useAppSelector } from './useRedux';
 
-const useLoading = (action: string): boolean => {
-  const loadingState = useAppSelector((state) => state.loader.loadingActions);
+// const useLoading = (action: string): boolean => {
+//   const loadingState = useAppSelector((state) => state.loader.loadingActions);
 
-  return loadingState.includes(action);
-};
+//   return loadingState.includes(action);
+// };
 
-export default useLoading;
+// export default useLoading;
