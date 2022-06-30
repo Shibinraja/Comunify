a1bb72f7fd1e0e6e10019f5ee4338fce798a5812
Armaan Mathew <armaan.mathew@neoito.com>
2022-06-30 18:31:55 +0530

    test commit for husky

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    node: true,
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  globals: {},
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'import', 'react-hooks'],
+  ignorePatterns: ['node_modules/'],
+  rules: {},
+  settings: {
+    react: {
+      version: 'detect', // "detect" automatically picks the version you have installed.
+    },
+  },
+};
