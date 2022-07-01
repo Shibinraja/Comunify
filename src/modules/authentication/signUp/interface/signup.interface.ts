ef660c572d9f4505b78175bd3d5cb5bf8d3a2853
shibinNeoito <shibin@neoito.com>
2022-07-01 14:27:54 +0530

    feat: Implementation of the user signUp and signIn functionality

--- src/modules/authentication/signUp/interface/signup.interface.ts
+++ src/modules/authentication/signUp/interface/signup.interface.ts
@@ -1,4 +1,31 @@
 export interface signUpInput {
+  email: string;
+  password: string;
+  userName: string;
+  companyName: string;
+  domainSector: string;
+}
 
-  }
-  
+export interface signUpResponseBody {
+  id: string;
+  email: string;
+  password: string;
+  userName: string;
+  companyName?: string;
+  domainSector?: string;
+  isVerified: boolean;
+  isAdmin: boolean;
+  profilePhotoUrl: string | null;
+  subscribtionPackageId: string | null;
+  googleId: string | null;
+  comunifyUserId: string;
+  planExpiry: string | null;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export type signUpResponse = {
+  error: boolean;
+  data: signUpResponseBody;
+  message: string;
+};
