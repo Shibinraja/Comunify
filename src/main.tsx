299bc334a646f26d1e4d905312f2eb37ebc9c803
Armaan Mathew <armaan.mathew@neoito.com>
2022-07-05 12:39:20 +0530

    strict mode removed

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { createRoot } from 'react-dom/client';
 let root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  //   <React.StrictMode>
+  <App />,
+  //   </React.StrictMode>,
 );
