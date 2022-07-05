c85f6215332150a256da5d4c85cb7e7e5be8d17d
harikrishnanjack007 <harikrishnan.p@neoito.com>
2022-07-05 15:23:50 +0530

    feat: Logout UI is added to the dashboard page

--- src/common/topBar/TopBar.tsx
+++ src/common/topBar/TopBar.tsx
@@ -6,8 +6,13 @@ import unplashmj from '../../assets/images/unsplash_mj.svg';
 import unionIcon from '../../assets/images/Union.svg';
 import sunIcon from '../../assets/images/sun.svg';
 import ellipseIcon from '../../assets/images/Ellipse 39.svg';
+import { useState } from 'react';
 
 function TopBar() {
+  
+  const [isDropdownActive, setisDropdownActive] = useState(false);
+  const options=['Profile Settings','Sign Out']
+
   return (
     <div className='container mt-6 mx-auto '>
       <div className='flex justify-between items-center '>
@@ -20,21 +25,29 @@ function TopBar() {
         </div>
         <img src={searchIcon} alt='' className='absolute pl-7' />
         <div className='flex items-center'>
-          <div>
+          <div className="cursor-pointer">
             <img src={sunIcon} alt='' />
           </div>
-          <div className='pl-1.68 relative'>
+          <div className='pl-1.68 relative cursor-pointer'>
             <img src={unionIcon} alt='' />
             <div className='absolute top-0 right-0 overflow-hidden'>
               <img src={ellipseIcon} alt='' />
             </div>
           </div>
-          <div className='pl-2.56'>
+          <div className='pl-2.56 relative'>
             <img
               src={profilePic}
               alt=''
-              className='rounded-full bg-cover bg-center'
+              className='rounded-full bg-cover bg-center relative cursor-pointer'
+              onClick={()=>setisDropdownActive(true)}
             />
+             {isDropdownActive && <div className="absolute border-box w-9.62 rounded-0.3 app-result-card-border bg-white cursor-pointer top-10 right-0 shadow-trialButtonShadow">
+               {options.map((options)=>
+                  <div className="flex flex-col" onClick={()=>setisDropdownActive(false)} key={options.toString()}>
+                      <div className="h-3.06 p-2 flex items-center text-searchBlack font-Poppins font-normal text-trial leading-1.31 hover:font-medium hover:bg-signUpDomain transition ease-in duration-300">{options}</div>
+                  </div>
+                )}
+            </div>}
           </div>
         </div>
       </div>
