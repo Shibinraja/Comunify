2816532eba5f2c6ecddc6c509934e29e2ee7d8a2
shibinNeoito <shibin@neoito.com>
2022-07-21 16:28:13 +0530

    fix: Comments added to loader component

--- src/routes/suspenseLoader.tsx
+++ src/routes/suspenseLoader.tsx
@@ -1,6 +1,8 @@
 import LoadingScreen from 'common/Loader/LoadingScreen';
 import { Suspense } from 'react';
 
+// Function to load fallbackUI when react renders the user component/ route based on it's priority.
+
 export const Loadable = (Component:any) => (props: JSX.IntrinsicAttributes) =>
   (
     <Suspense fallback={<LoadingScreen />}>
