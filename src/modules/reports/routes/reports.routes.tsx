0dd4389409a124ff77088c50660d50e41fec3e2d
Armaan Mathew <armaan.mathew@neoito.com>
2022-07-22 17:10:35 +0530

    loadable added in report routes

--- src/modules/reports/routes/reports.routes.tsx
+++ src/modules/reports/routes/reports.routes.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { RoutesArray } from '../../../interface/interface';
+import { Loadable } from 'routes/suspenseLoader';
 
-const Report = React.lazy(() => import('../pages/Report'));
-const CreateReport = React.lazy(()=> import('../pages/createReport/CreateReport'));
+
+const Report = Loadable(React.lazy(() => import('../pages/Report')))
+const CreateReport = Loadable(React.lazy(()=> import('../pages/createReport/CreateReport')))
 
 const reportRoutes: RoutesArray = {
     path: 'reports',
