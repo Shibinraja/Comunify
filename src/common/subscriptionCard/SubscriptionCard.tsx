a9af45d214fa9ca5adbf0e8e01aa182333b9d045
harikrishnanjack007 <harikrishnan.p@neoito.com>
2022-07-28 11:41:17 +0530

    fix: Doller Symbol is added in welcome screen

--- src/common/subscriptionCard/SubscriptionCard.tsx
+++ src/common/subscriptionCard/SubscriptionCard.tsx
@@ -18,7 +18,7 @@ const SubscriptionCard: React.FC<SubscriptionProps> = ({ subscriptionData }) => 
       <div key={subscriptionData?.id} className="mt-1.87  flex flex-col ">
         <div className="border-gradient-rounded px-8 py-5 bg-white rounded-0.9 ">
           <h5 className="flex items-center">
-            <span className="price font-Poppins font-semibold text-renewalPrice leading-3.1">{subscriptionData?.cost}</span>
+            <span className="price font-Poppins font-semibold text-renewalPrice leading-3.1"><span>$</span>{subscriptionData?.cost}</span>
             <span className="font-Poppins font-medium text-slimGray text-base leading-6"> /month</span>{' '}
           </h5>
           <h6 className="pt-0.43 font-Poppins text-infoBlack text-base font-semibold leading-6">{subscriptionData?.name}</h6>
