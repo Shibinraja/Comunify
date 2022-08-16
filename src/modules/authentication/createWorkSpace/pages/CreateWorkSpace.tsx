40f2f09664549f2c2e3a8986031e70bc79151fcb
harikrishnanjack007 <harikrishnan.p@neoito.com>
2022-08-16 23:38:19 +0530

    fix: Workspace Layout initial fix

--- src/modules/authentication/createWorkSpace/pages/CreateWorkSpace.tsx
+++ src/modules/authentication/createWorkSpace/pages/CreateWorkSpace.tsx
@@ -1,8 +1,8 @@
-import { useAppDispatch } from '@/hooks/useRedux';
+import { useAppDispatch, useAppSelector } from '@/hooks/useRedux';
 import Button from 'common/button/Button';
 import Input from 'common/input/Input';
 import authSlice from 'modules/authentication/store/slices/auth.slice';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as Yup from 'yup';
 import bgWorkSpaceImage from '../../../../assets/images/bg-sign.svg';
 import { AppDispatch } from '../../../../store/index';
@@ -10,11 +10,16 @@ import './CreateWorkSpace.css';
 
 const CreateWorkSpace: React.FC = () => {
   const dispatch: AppDispatch = useAppDispatch();
+  const { workspaceData } = useAppSelector((state) => state.auth);
 
   const [workspaceName, setWorkspaceName] = useState<string>('');
   const [errorMessage, setErrorMessage] = useState<string | unknown>('');
 
   const workspaceNameValidation = Yup.string().min(4, 'Workspace Name must be atleast 4 characters');
+
+  useEffect(() => {
+    dispatch(authSlice.actions.getWorkspace());
+  }, []);
 
   const handleWorkspaceName = (e: React.ChangeEvent<HTMLInputElement>): void => {
     const workspace_name = e.target.value;
@@ -38,36 +43,39 @@ const CreateWorkSpace: React.FC = () => {
         <div className="w-1/2 rounded-r-lg  bg-thinBlue flex items-center justify-center p-28 signup-cover-bg bg-no-repeat bg-left overflow-hidden">
           <img src={bgWorkSpaceImage} alt="" className="object-cover" />
         </div>
-        <div className="flex flex-col w-1/2 pl-7.53 pt-6.84 overflow-scroll">
-          {' '}
-          <h3 className="font-Inter text-neutralBlack font-bold not-italic text-signIn leading-2.8">Create Workspace </h3>{' '}
-          <form
-            className="flex flex-col pb-10 mt-1.8 w-25.9 "
-            autoComplete="off"
-            onSubmit={(e) => {
-              handleSubmit(e);
-            }}
-          >
-            <div className="workspace">
-              <Input
-                type="text"
-                placeholder="Enter the Workspace Name"
-                label="Workspace"
-                id="workspaceName"
-                name="workspaceName"
-                value={workspaceName}
-                errors={Boolean(errorMessage)}
-                helperText={errorMessage}
-                className="h-4.5 rounded-lg bg-white p-2.5 focus:outline-none placeholder:font-normal placeholder:text-secondaryGray placeholder:text-base placeholder:leading-6 placeholder:font-Inter shadow-trialButtonShadow font-Inter box-border"
-                onChange={handleWorkspaceName}
+        <div className="flex justify-center items-center  w-1/2 ">
+          <div className="flex flex-col overflow-scroll">
+            {' '}
+            <h3 className="font-Inter text-neutralBlack font-bold not-italic text-signIn leading-2.8">Create Workspace </h3>{' '}
+            <form
+              className="flex flex-col pb-10 mt-1.8 w-25.9 "
+              autoComplete="off"
+              onSubmit={(e) => {
+                handleSubmit(e);
+              }}
+            >
+              <div className="workspace">
+                <Input
+                  type="text"
+                  placeholder="Enter the Workspace Name"
+                  label="Workspace"
+                  id="workspaceName"
+                  name="workspaceName"
+                  value={workspaceName}
+                  errors={Boolean(errorMessage)}
+                  helperText={errorMessage}
+                  className="h-4.5 rounded-lg bg-white p-2.5 focus:outline-none placeholder:font-normal placeholder:text-secondaryGray placeholder:text-base placeholder:leading-6 placeholder:font-Inter shadow-trialButtonShadow font-Inter box-border"
+                  onChange={handleWorkspaceName}
+                />
+              </div>
+              <Button
+                disabled={Boolean(workspaceData?.length !== 0) || Boolean(errorMessage) || !workspaceName}
+                text="Confirm"
+                type="submit"
+                className="font-Poppins rounded-lg text-base font-semibold text-white mt-1.8 h-3.6 transition ease-in duration-300 hover:shadow-buttonShadowHover btn-gradient"
               />
-            </div>
-            <Button
-              text="Confirm"
-              type="submit"
-              className="font-Poppins rounded-lg text-base font-semibold text-white mt-1.8 h-3.6 transition ease-in duration-300 hover:shadow-buttonShadowHover btn-gradient"
-            />
-          </form>
+            </form>
+          </div>
         </div>
       </div>
     </div>
