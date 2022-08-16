27b7b0158b1025aad4d558decb172d2a5b0fb896
harikrishnanjack007 <harikrishnan.p@neoito.com>
2022-08-16 11:28:07 +0530

    fix: Signup Layout initial fix

--- src/modules/authentication/signUp/pages/SignUp.tsx
+++ src/modules/authentication/signUp/pages/SignUp.tsx
@@ -101,155 +101,163 @@ const SignUp: React.FC = () => {
 
   return (
     <div className="sign-up">
-      <div className="flex w-full height-calc">
+      <div className="flex w-full height-calc ">
         <div className="w-1/2 rounded-r-lg  bg-thinBlue flex items-center justify-center p-28 signup-cover-bg bg-no-repeat bg-left overflow-hidden">
           <img src={bgSignUpImage} alt="" className="object-cover" />
         </div>
-        <div className="w-1/2 flex flex-col pl-7.5 overflow-scroll">
-          <h3 className="font-Inter text-neutralBlack font-bold not-italic text-signIn leading-2.8">Sign Up </h3>{' '}
-          <p className="text-lightGray font-Inter  max-w-sm font-normal not-italic mt-0.78 text-desc">
-            Get Comunified with your communities. Create your account now.
-          </p>
-          <Formik innerRef={formikRef} initialValues={initialValues} onSubmit={handleSubmit} validateOnChange={true} validationSchema={signUpSchema}>
-            {({ errors, handleBlur, handleChange, handleSubmit, touched, values }): JSX.Element => (
-              <Form className="flex flex-col pb-10 mt-1.8 w-25.9" autoComplete="off" onSubmit={handleSubmit}>
-                <div className="username">
-                  <Input
-                    type="text"
-                    placeholder="Username"
-                    label="Username"
-                    id="username"
-                    name="userName"
-                    className="h-4.5 rounded-lg bg-white p-2.5 focus:outline-none placeholder:font-normal placeholder:text-secondaryGray placeholder:text-base placeholder:leading-6 font-Inter box-border"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    value={values.userName}
-                    errors={Boolean(touched.userName && errors.userName)}
-                    helperText={touched.userName && errors.userName}
-                  />
-                </div>
-                <div className="email mt-1.258">
-                  <Input
-                    type="email"
-                    placeholder="Email"
-                    label="Email"
-                    id="email"
-                    name="email"
-                    className="h-4.5 rounded-lg bg-white p-2.5 focus:outline-none placeholder:font-normal placeholder:text-secondaryGray placeholder:text-base placeholder:leading-6 font-Inter box-border"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    value={values.email}
-                    errors={Boolean(touched.email && errors.email)}
-                    helperText={touched.email && errors.email}
-                  />
-                </div>
-                <div className="w-full password mt-1.258 relative">
-                  <Input
-                    type={passwordType}
-                    placeholder="Create Password"
-                    label="Password"
-                    id="password"
-                    name="password"
-                    className="h-4.5 rounded-lg pr-3.12 bg-white p-2.5 focus:outline-none placeholder:font-normal placeholder:text-secondaryGray placeholder:text-base placeholder:leading-6 font-Inter box-border "
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    value={values.password}
-                    errors={Boolean(touched.password && errors.password)}
-                    helperText={touched.password && errors.password}
-                  />
-                  <div onClick={togglePassword} className="absolute top-7 right-[26.87px]">
-                    {passwordType === 'password' ? (
-                      <img className="cursor-pointer " src={eyeIcon} alt="" />
-                    ) : (
-                      <img className="cursor-pointer " src={closeEyeIcon} alt="" />
-                    )}
+        <div className="flex justify-center w-1/2 ">
+          <div className="flex flex-col overflow-scroll">
+            <h3 className="font-Inter text-neutralBlack font-bold not-italic text-signIn leading-2.8">Sign Up </h3>{' '}
+            <p className="text-lightGray font-Inter  max-w-sm font-normal not-italic mt-0.78 text-desc">
+              Get Comunified with your communities. Create your account now.
+            </p>
+            <Formik
+              innerRef={formikRef}
+              initialValues={initialValues}
+              onSubmit={handleSubmit}
+              validateOnChange={true}
+              validationSchema={signUpSchema}
+            >
+              {({ errors, handleBlur, handleChange, handleSubmit, touched, values }): JSX.Element => (
+                <Form className="flex flex-col pb-10 mt-1.8 w-25.9" autoComplete="off" onSubmit={handleSubmit}>
+                  <div className="username">
+                    <Input
+                      type="text"
+                      placeholder="Username"
+                      label="Username"
+                      id="username"
+                      name="userName"
+                      className="h-4.5 rounded-lg bg-white p-2.5 focus:outline-none placeholder:font-normal placeholder:text-secondaryGray placeholder:text-base placeholder:leading-6 font-Inter box-border"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      value={values.userName}
+                      errors={Boolean(touched.userName && errors.userName)}
+                      helperText={touched.userName && errors.userName}
+                    />
                   </div>
-                </div>
-                <div className="cname mt-1.258">
-                  <Input
-                    type="text"
-                    placeholder="Company Name"
-                    label="Company Name"
-                    id="cname"
-                    name="companyName"
-                    className="h-4.5 rounded-lg bg-white p-2.5 focus:outline-none placeholder:font-normal placeholder:text-secondaryGray placeholder:text-base placeholder:leading-6 font-Inter box-border"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    value={values.companyName}
-                    errors={Boolean(touched.companyName && errors.companyName)}
-                    helperText={touched.companyName && errors.companyName}
-                  />
-                </div>
-                <div className="domain mt-1.258 relative">
-                  <div className="cursor-pointer ">
-                    <div
-                      className="flex items-center w-full  justify-between app-result-card-border  box-border rounded-lg h-4.5  bg-white p-2.5 focus:outline-none font-normal text-secondaryGray text-base leading-6 font-Inter shadow-trialButtonShadow relative"
-                      ref={dropDownRef}
-                      onClick={() => setDropDownActive(!isDropDownActive)}
-                    >
-                      <div className={selectedDomainSector === 'Domain' ? 'text-secondaryGray' : 'text-black'}>
-                        {selectedDomainSector ? selectedDomainSector : 'Domain'}
-                      </div>
-                      <img src={dropdownIcon} alt="" className={isDropDownActive ? 'rotate-180' : 'rotate-0'} />
+                  <div className="email mt-1.258">
+                    <Input
+                      type="email"
+                      placeholder="Email"
+                      label="Email"
+                      id="email"
+                      name="email"
+                      className="h-4.5 rounded-lg bg-white p-2.5 focus:outline-none placeholder:font-normal placeholder:text-secondaryGray placeholder:text-base placeholder:leading-6 font-Inter box-border"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      value={values.email}
+                      errors={Boolean(touched.email && errors.email)}
+                      helperText={touched.email && errors.email}
+                    />
+                  </div>
+                  <div className="w-full password mt-1.258 relative">
+                    <Input
+                      type={passwordType}
+                      placeholder="Create Password"
+                      label="Password"
+                      id="password"
+                      name="password"
+                      className="h-4.5 rounded-lg pr-3.12 bg-white p-2.5 focus:outline-none placeholder:font-normal placeholder:text-secondaryGray placeholder:text-base placeholder:leading-6 font-Inter box-border "
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      value={values.password}
+                      errors={Boolean(touched.password && errors.password)}
+                      helperText={touched.password && errors.password}
+                    />
+                    <div onClick={togglePassword} className="absolute top-7 right-[26.87px]">
+                      {passwordType === 'password' ? (
+                        <img className="cursor-pointer " src={eyeIcon} alt="" />
+                      ) : (
+                        <img className="cursor-pointer " src={closeEyeIcon} alt="" />
+                      )}
                     </div>
-
-                    {isDropDownActive && (
-                      <div className="absolute w-full bg-white app-result-card-border box-border rounded-0.3 shadow-reportInput">
-                        <ul id="domain" className="flex flex-col justify-center">
-                          {options.map((options: string, index: number) => (
-                            <li
-                              ref={domainRef}
-                              onKeyDown={handleKeyDown}
-                              tabIndex={0}
-                              key={options}
-                              onClick={() => {
-                                handleDomainSectorChange(options);
-                                setDropDownActive(false);
-                              }}
-                              className={`${
-                                cursor === index ? 'bg-signUpDomain' : null
-                              } h-3.06 font-Poppins font-normal text-searchBlack text-trial leading-1.31 flex items-center p-3 hover:bg-signUpDomain transition ease-in duration-100 focus:outline-none`}
-                            >
-                              {options}
-                            </li>
-                          ))}
-                        </ul>
+                  </div>
+                  <div className="cname mt-1.258">
+                    <Input
+                      type="text"
+                      placeholder="Company Name"
+                      label="Company Name"
+                      id="cname"
+                      name="companyName"
+                      className="h-4.5 rounded-lg bg-white p-2.5 focus:outline-none placeholder:font-normal placeholder:text-secondaryGray placeholder:text-base placeholder:leading-6 font-Inter box-border"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      value={values.companyName}
+                      errors={Boolean(touched.companyName && errors.companyName)}
+                      helperText={touched.companyName && errors.companyName}
+                    />
+                  </div>
+                  <div className="domain mt-1.258 relative">
+                    <div className="cursor-pointer ">
+                      <div
+                        className="flex items-center w-full  justify-between app-result-card-border  box-border rounded-lg h-4.5  bg-white p-2.5 focus:outline-none font-normal text-secondaryGray text-base leading-6 font-Inter shadow-trialButtonShadow relative"
+                        ref={dropDownRef}
+                        onClick={() => setDropDownActive(!isDropDownActive)}
+                      >
+                        <div className={selectedDomainSector === 'Domain' ? 'text-secondaryGray' : 'text-black'}>
+                          {selectedDomainSector ? selectedDomainSector : 'Domain'}
+                        </div>
+                        <img src={dropdownIcon} alt="" className={isDropDownActive ? 'rotate-180' : 'rotate-0'} />
                       </div>
+
+                      {isDropDownActive && (
+                        <div className="absolute w-full bg-white app-result-card-border box-border rounded-0.3 shadow-reportInput">
+                          <ul id="domain" className="flex flex-col justify-center">
+                            {options.map((options: string, index: number) => (
+                              <li
+                                ref={domainRef}
+                                onKeyDown={handleKeyDown}
+                                tabIndex={0}
+                                key={options}
+                                onClick={() => {
+                                  handleDomainSectorChange(options);
+                                  setDropDownActive(false);
+                                }}
+                                className={`${
+                                  cursor === index ? 'bg-signUpDomain' : null
+                                } h-3.06 font-Poppins font-normal text-searchBlack text-trial leading-1.31 flex items-center p-3 hover:bg-signUpDomain transition ease-in duration-100 focus:outline-none`}
+                              >
+                                {options}
+                              </li>
+                            ))}
+                          </ul>
+                        </div>
+                      )}
+                    </div>
+                    {Boolean(touched.domainSector && errors.domainSector) && (
+                      <p className="text-lightRed font-normal text-email absolute font-Inter mt-0.287  pl-1">{errors?.domainSector}</p>
                     )}
                   </div>
-                  {Boolean(touched.domainSector && errors.domainSector) && (
-                    <p className="text-lightRed font-normal text-email absolute font-Inter mt-0.287  pl-1">{errors?.domainSector}</p>
-                  )}
-                </div>
-                <Button
-                  text="Sign Up"
-                  type="submit"
-                  className="font-Poppins rounded-lg text-base font-semibold text-white mt-1.8 h-3.6 transition ease-in duration-300 hover:shadow-buttonShadowHover btn-gradient"
-                />
-                <div className="relative flex items-center pt-2.4 -z-40">
-                  <div className="borders flex-grow border-t"></div>
-                  <span className="font-Inter text-secondaryGray mx-6 flex-shrink">or</span>
-                  <div className="borders flex-grow border-t"></div>
-                </div>
-                <div
-                  className="google-signin h-3.3 mt-2.47 font-Inter text-lightBlue box-border flex text-desc  cursor-pointer items-center justify-center rounded-lg font-normal leading-2.8"
-                  onClick={navigateToGoogleSignIn}
-                >
-                  <img src={socialLogo} alt="" className="pr-0.781" />
-                  Continue with Google
-                </div>
-                <div className="font-Poppins text-secondaryGray text-center text-base font-normal mt-1.8  text-signLink">
-                  <h3>
-                    Already have an account?{' '}
-                    <Link to="/">
-                      {' '}
-                      <span className="text-letsSignInSignUp underline">Let’s Sign In</span>
-                    </Link>{' '}
-                  </h3>
-                </div>
-              </Form>
-            )}
-          </Formik>
+                  <Button
+                    text="Sign Up"
+                    type="submit"
+                    className="font-Poppins rounded-lg text-base font-semibold text-white mt-1.8 h-3.6 transition ease-in duration-300 hover:shadow-buttonShadowHover btn-gradient"
+                  />
+                  <div className="relative flex items-center pt-2.4 -z-40">
+                    <div className="borders flex-grow border-t"></div>
+                    <span className="font-Inter text-secondaryGray mx-6 flex-shrink">or</span>
+                    <div className="borders flex-grow border-t"></div>
+                  </div>
+                  <div
+                    className="google-signin h-3.3 mt-2.47 font-Inter text-lightBlue box-border flex text-desc  cursor-pointer items-center justify-center rounded-lg font-normal leading-2.8"
+                    onClick={navigateToGoogleSignIn}
+                  >
+                    <img src={socialLogo} alt="" className="pr-0.781" />
+                    Continue with Google
+                  </div>
+                  <div className="font-Poppins text-secondaryGray text-center text-base font-normal mt-1.8  text-signLink">
+                    <h3>
+                      Already have an account?{' '}
+                      <Link to="/">
+                        {' '}
+                        <span className="text-letsSignInSignUp underline">Let’s Sign In</span>
+                      </Link>{' '}
+                    </h3>
+                  </div>
+                </Form>
+              )}
+            </Formik>
+          </div>
         </div>
       </div>
     </div>
