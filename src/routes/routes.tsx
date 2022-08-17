87bae22d15d1a333fbdfa621972da3993f92b5d7
Armaan Mathew <armaan.mathew@neoito.com>
2022-08-17 07:50:11 +0530

    routes error fixed

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -10,8 +10,19 @@ import membersRoutes from '../modules/members/routes/members.routes';
 import PrivateRoute from './PrivateRoute';
 import Loadable from './suspenseLoader';
 import accountRoutes from 'modules/account/routes/account.routes';
+import { getLocalWorkspaceId } from '../lib/helper';
+import { getLocalRefreshToken } from '../lib/request';
+import { DecodeToken } from '../modules/authentication/interface/auth.interface';
+import { decodeToken } from '@/lib/decodeToken';
+import cookie from 'react-cookies';
 
 const MainLayout = Loadable(lazy(() => import('../layout/MainLayout')));
+
+const tokenData = getLocalRefreshToken();
+
+const access_token = tokenData || cookie.load('x-auth-cookie');
+const decodedToken: DecodeToken = access_token && decodeToken(access_token);
+const workspaceId = getLocalWorkspaceId() || decodedToken?.workspaceId;
 
 const routes: RoutesArray[] = [
   ...authRoutes,
@@ -24,7 +35,7 @@ const routes: RoutesArray[] = [
     ),
     children: [
       {
-        path: ':workspaceId',
+        path: `${workspaceId}`,
         children: [dashboardRoutes, membersRoutes, settingRoutes, activityRoutes, reportRoutes, accountRoutes]
       }
     ]
