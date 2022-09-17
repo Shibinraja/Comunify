51ec5a58d2848adaf9fc5a340305a4c353bd713e
aakash <58246892+aakashtm@users.noreply.github.com>
2022-09-17 17:50:32 +0530

    Develop (#302)
    * date picker width fixed
    
    * Theme dark mode integration on dashboard
    
    * img tag removed
    
    * removed unwanted files
    
    * setting page started dark theme implimentation
    
    * setting page dark theme integretion
    
    * setting page tabs and report page dark theme integretion
    
    * bugfix: tag fixes after backend change
    
    * bugfix: type fix
    
    * fix: Activity Modal popup issue got fixed
    
    * fix: pagination issue in pagination component fixed
    
    * infinity scroll filter issues and member profile card platform not showing issue also fixed (#263)
    
    * infinity scroll filter issues and member profile card platform not showing fixed
    
    * activity.tsx linting issue fixed
    
    * fix: issue fixes in the members and active stream component
    
    * Date filter fixed in member profile infinte scroll (#265)
    
    * infinity scroll filter issues and member profile card platform not showing fixed
    
    * activity.tsx linting issue fixed
    
    * fix: Date filter fixed in member profile infinte scroll
    
    * fix: filter pop up dropdown close issue fixed
    
    * Fixed date filter issues in members page infinity scroll. Also fixed filter modal not closing issue
    
    * fix: Image size issue fixed
    
    * sidenav theme added
    
    * Hari/hotfix/coma 592 - Integration Vanilla (#271)
    
    * fix: Integration and logo related issues fixed
    
    * fix: Integration and logo related issues fixed
    
    * Added discord as a coming soon platform. Also made changes to vanilla popup access token link to open in a tab
    
    * fix: Bug fixes in active stream and members with update in pagination
    
    * Fixed activity time issue. Also added better checks for condtions for integration (#275)
    
    * activity time fix
    
    * chnages in activity.tsx
    
    * fix: export issue in active stream and members fixed
    
    * fix: active stream search issue fixes
    
    * fixes in pagination
    
    * fix: bug fixes in active stream and memberslist (#286)
    
    * Shibin/fix/coma 583 activity table issues (#291)
    
    * fix: activity table issues related to combintaion filter with pagination is fixed
    
    * fix: Merge conflict issue resolved
    
    * fix: days filter issue in members component issue fixed (#292)
    
    * dark theme hide
    
    * fix: merge conflicts solved
    
    * fix: date filter auto clear issue solved (#294)
    
    * UI: Merged members UI added
    
    * merge modal design check
    
    * fix: merge modal issue fixed
    
    * feat: Merge Member integrated
    
    * fix: Comments added
    
    * fix: Members selection issue fixed
    
    * feat: Widget screen Initial setup (#297)
    
    * Shibin/feat/coma 635 unmerge members (#301)
    
    * UI: Merged members UI added
    
    * merge modal design check
    
    * fix: merge modal issue fixed
    
    * feat: Merge Member integrated
    
    * fix: Comments added
    
    * feat: Un merge members api and functionality integrated
    
    * fix: removal of merge-memberId from store
    
    Co-authored-by: harikrishnanjack007 <harikrishnan.p@neoito.com>
    Co-authored-by: armaani69 <89074075+armaani69@users.noreply.github.com>
    
    * fix: Tag validation displaying issue fixed (#299)
    
    Co-authored-by: Paulson Ps <paulsonps@neoito.com>
    Co-authored-by: vivekb1702 <vivek.b@neoito.com>
    Co-authored-by: harikrishnanjack007 <harikrishnan.p@neoito.com>
    Co-authored-by: shibinNeoito <shibin@neoito.com>
    Co-authored-by: armaani69 <89074075+armaani69@users.noreply.github.com>
    Co-authored-by: Armaan Mathew <armaan.mathew@neoito.com>
    Co-authored-by: Shibin Raja <93582189+shibinNeoito@users.noreply.github.com>

--- src/modules/members/interface/members.interface.ts
+++ src/modules/members/interface/members.interface.ts
@@ -160,11 +160,41 @@ export interface ActivityResult {
   updatedAt: Date;
 }
 
+export type platformLogoType = {
+  platformLogoUrl: string;
+};
 export interface ActivityDataResponse {
   result: ActivityResult[];
   nextCursor: string | null;
 }
 
+export type MergeMembersDataResult = {
+  id: string;
+  workspaceId: string;
+  name: string;
+  platformName: string;
+  organization: string;
+  location: string;
+  comunifyMemberId: string;
+  email: string;
+  profileUrl: string;
+  isMerged: boolean;
+  parentMemberId: null | string;
+  isPrimary: boolean;
+  platformId: string;
+  platformMemberId: string;
+  joinedAt: string;
+  createdAt: string;
+  updatedAt: string;
+  platform: {
+    platformLogoUrl: string;
+  };
+};
+
+export type MergeMembersDataResponse = {
+  result: MergeMembersDataResult[];
+  nextCursor: string | null;
+};
 export interface ActivityInfiniteScroll {
   workspaceId: string;
   memberId: string;
@@ -237,10 +267,13 @@ export interface MemberProfileCard {
   platformMemberId: string;
   createdAt: Date | string;
   updatedAt: Date | string;
-  platforms: {
+  platforms?: {
     id: string;
     name: string;
     platformLogoUrl: string;
   }[];
   tags: TagResponseData[];
+  platform?: {
+    platformLogoUrl: string;
+  };
 }
