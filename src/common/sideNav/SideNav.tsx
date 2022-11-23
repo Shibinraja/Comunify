4bac8bd1d79b225a4c05a1649ae3cb06938aa78f
Paulsonps <paulsonps@neoito.com>
2022-11-23 15:02:03 +0530

    menu items height fixed

--- src/common/sideNav/SideNav.tsx
+++ src/common/sideNav/SideNav.tsx
@@ -96,7 +96,8 @@ const SideNav: React.FC = () => {
             <div className="pl-0.66 font-Outfit font-bold text-dashboardLogo text-lightBlack dark:text-white leading-1.43">COMUNIFY</div>
           </NavLink>
         </div>
-        <div className="flex flex-col mt-5.8 menu-box overflow-y-auto">
+        <div
+          className={subscriptionDetails?.subscriptionPackage?.name?.toLocaleLowerCase().trim() === 'free trial' ? 'flex flex-col mt-5.8 menu-box overflow-y-auto' : 'flex flex-col mt-5.8 menu-box-items overflow-y-auto'}>
           <div className={window.location.href.includes('/dashboard') ? 'flex flex-center active-menu' : 'flex flex-center inactive-menu'}>
             <NavLink to={`${workspaceId}/dashboard`}>
               <div>
@@ -107,8 +108,7 @@ const SideNav: React.FC = () => {
             <NavLink
               to={`${workspaceId}/dashboard`}
               className={({ isActive }) =>
-                `pl-1.24 font-Poppins font-medium text-base xl:text-lg  leading-1.68 cursor-pointer ${
-                  isActive ? 'text-black dark:text-white' : 'text-tableDuration'
+                `pl-1.24 font-Poppins font-medium text-base xl:text-lg  leading-1.68 cursor-pointer ${isActive ? 'text-black dark:text-white' : 'text-tableDuration'
                 }`
               }
             >
@@ -124,8 +124,7 @@ const SideNav: React.FC = () => {
             <NavLink
               to={`${workspaceId}/members`}
               className={({ isActive }) =>
-                `pl-1.24 font-Poppins font-medium text-base xl:text-lg  leading-1.68 cursor-pointer ${
-                  isActive ? 'text-black dark:text-white' : 'text-tableDuration'
+                `pl-1.24 font-Poppins font-medium text-base xl:text-lg  leading-1.68 cursor-pointer ${isActive ? 'text-black dark:text-white' : 'text-tableDuration'
                 }`
               }
             >
@@ -141,8 +140,7 @@ const SideNav: React.FC = () => {
             <NavLink
               to={`${workspaceId}/activity`}
               className={({ isActive }) =>
-                `pl-1.24 font-Poppins font-medium text-base xl:text-lg  leading-1.68 cursor-pointer ${
-                  isActive ? 'text-black dark:text-white' : 'text-tableDuration'
+                `pl-1.24 font-Poppins font-medium text-base xl:text-lg  leading-1.68 cursor-pointer ${isActive ? 'text-black dark:text-white' : 'text-tableDuration'
                 }`
               }
             >
@@ -158,8 +156,7 @@ const SideNav: React.FC = () => {
             <NavLink
               to={`${workspaceId}/reports`}
               className={({ isActive }) =>
-                `pl-1.24 font-Poppins font-medium text-base xl:text-lg  leading-1.68 cursor-pointer ${
-                  isActive ? 'text-black dark:text-white' : 'text-tableDuration'
+                `pl-1.24 font-Poppins font-medium text-base xl:text-lg  leading-1.68 cursor-pointer ${isActive ? 'text-black dark:text-white' : 'text-tableDuration'
                 }`
               }
             >
@@ -175,8 +172,7 @@ const SideNav: React.FC = () => {
             <NavLink
               to={`${workspaceId}/settings`}
               className={({ isActive }) =>
-                `pl-1.24 font-Poppins font-medium text-base xl:text-lg  leading-1.68 cursor-pointer ${
-                  isActive ? 'text-black dark:text-white' : 'text-tableDuration'
+                `pl-1.24 font-Poppins font-medium text-base xl:text-lg  leading-1.68 cursor-pointer ${isActive ? 'text-black dark:text-white' : 'text-tableDuration'
                 }`
               }
             >
